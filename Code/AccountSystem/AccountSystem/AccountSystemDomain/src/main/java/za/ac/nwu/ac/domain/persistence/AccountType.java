54d01c28d5d15e854225266fe04706e0dbe5a198
Exley <exley.roux@gmail.com>
2021-10-06 19:35:43 +0200

    Start with database connections setup

---
+++ Code/AccountSystem/AccountSystem/AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
@@ -0,0 +1,10 @@
+package za.ac.nwu.ac.domain.persistence;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account_types", schema = "")
+public class AccountType {
+}
--- Code/AccountSystem/AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/DemoController.java
+++ Code/AccountSystem/AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/DemoController.java
@@ -1,15 +1,68 @@
 package za.ac.nwu.ac.web.sb.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import za.ac.nwu.ac.domain.service.GeneralResponse;
 
 @RestController
+@RequestMapping("demo")
 public class DemoController {
 
     @GetMapping("/ping")
-    public GeneralResponse<String> ping(@RequestParam(value = "echo", defaultValue = "pong") String echo){
-        return new GeneralResponse<String>(echo);
+    @ApiOperation(value = "Echo the Ping.", notes = "This echo the ping back to the client")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The Ping was received and echoed", response = GeneralResponse.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+            @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)
+    })
+    public ResponseEntity<GeneralResponse<String>> ping(@RequestParam(value = "echo", defaultValue = "pong") String echo){
+                GeneralResponse<String> response = new GeneralResponse<>(echo);
+                return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("")
+    @ApiOperation(value = "Echo the Ping.", notes = "This echo the ping back to the client")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The Ping was received and echoed", response = GeneralResponse.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+            @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)
+    })
+    public ResponseEntity<GeneralResponse<String>> getPing(@RequestParam(value = "echo", defaultValue = "pong") String echo){
+        GeneralResponse<String> response = new GeneralResponse<>(echo);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+
+//    @PostMapping("/ping")
+//    @ApiOperation(value = "Creates a new Pong", notes = "Creates new Pong in the System")
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 201, message = "The Ping was received and echoed", response = GeneralResponse.class),
+//            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+//            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)
+//    })
+//    public ResponseEntity<GeneralResponse<Pong>> postPing(@ApiParam(value = "Request body to make a new Pong.", required = true) @RequestBody Pong pong){
+//        GeneralResponse<Pong> response = new GeneralResponse<Pong>(pong);
+//        return new ResponseEntity<>(response, HttpStatus.CREATED);
+//    }
+
+    @GetMapping("/error")
+    @ApiOperation(value = "Throws an eception")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The Ping was received and echoed", response = GeneralResponse.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+            @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)
+    })
+    public ResponseEntity<GeneralResponse<String>> ping(){
+        throw new RuntimeException("Give an error");
     }
 }
