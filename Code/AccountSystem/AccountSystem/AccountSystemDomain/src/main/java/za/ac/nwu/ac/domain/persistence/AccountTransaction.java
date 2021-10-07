b974f90ed80d910e5f9dd46e28b601306bf5be98
Exley <exley.roux@gmail.com>
2021-10-07 03:17:35 +0200

    Added Database Entity Classes

--- Code/AccountSystem/AccountSystem/AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
+++ Code/AccountSystem/AccountSystem/AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
@@ -1,4 +1,87 @@
 package za.ac.nwu.ac.domain.persistence;
 
-public class AccountTransaction {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "account_types", schema = "account_system")
+public class AccountTransaction implements Serializable {
+
+    private Long transactionId;
+    private Long accountTypeId;
+    private Long memberId;
+    private Long amount;
+    private LocalDate transactionDate;
+
+    public AccountTransaction(Long transactionId, Long accountTypeId, Long memberId, Long amount, LocalDate transactionDate) {
+        this.transactionId = transactionId;
+        this.accountTypeId = accountTypeId;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+    }
+
+    public AccountTransaction() {
+    }
+
+    @Id
+    @Column(name = "ID_TRANSACTION")
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Column(name = "ID_ACCOUNT_TYPE")
+    public Long getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(Long accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
+
+    @Column(name = "ID_USER")
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    @Column(name = "AMOUNT")
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    @Column(name = "DATE_TRANSACTION")
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountTransaction that = (AccountTransaction) o;
+        return Objects.equals(transactionId, that.transactionId) && Objects.equals(accountTypeId, that.accountTypeId) && Objects.equals(memberId, that.memberId) && Objects.equals(amount, that.amount) && Objects.equals(transactionDate, that.transactionDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, accountTypeId, memberId, amount, transactionDate);
+    }
 }
